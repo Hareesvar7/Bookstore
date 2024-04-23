9404a231386e2154d7219c6aab36207a92cd26e9
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-04-23 17:41:06 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://hareesvar1:hareesvar1@book.hwrzihl.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book';
+
+  
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
